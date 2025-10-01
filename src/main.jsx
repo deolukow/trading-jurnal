4ee92d7ba79edd0dca5da0192f1811effe726793
deolukow <lakabambam07@gmail.com>
2025-10-01 22:45:21 +0800

    setup PWA with vite-plugin-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 🟢 Tambahkan ini
+import { registerSW } from 'virtual:pwa-register'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// 🟢 Registrasi service worker PWA
+registerSW({
+  immediate: true, // langsung aktif begitu selesai build
+})
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,36 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'WzGold Trading Jurnal',
+        short_name: 'WzJurnal',
+        description: 'Aplikasi jurnal trading terbaik yang pernah saya dapati',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })],
+  base: '/trading-jurnal/',
 })
